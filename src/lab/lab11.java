e2d58050bb4e431683abec98cbfef504073f5623
Admin <Admin@DESKTOP-L8F1SQD>
2023-04-12 14:19:31 +0800

    sysout

---
+++ src/lab/lab11.java
@@ -0,0 +1,12 @@
+package lab;
+
+public class lab11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hi");
+		
+		System.out.println("kdgjklfshgl");
+	}
+
+}
